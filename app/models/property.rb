81c33b8ffba89dd3b548e433858e9bbe0fe74963
Ricardo Cuevas <rcuevasjr91@gmail.com>
2017-03-01 11:50:52 -0500

    Property Model variables added

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,13 @@
 class Property < ApplicationRecord
   belongs_to :company
+
+  @vacant = Property.where(group: "Vacant Unrented")
+  @notice = Property.where(group: "Notice Unrented")
+  @available = @vacant + @notice
+  #@available.each { |i| }
+
+  #def spread(price)
+    #@spread = price * .025
+#  end
+
 end
