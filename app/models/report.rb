e9b5a7c48c8bc3d2e8278ec0b65ef6e23c3c6d6d
Ricardo Cuevas <rcuevasjr91@gmail.com>
2017-03-01 20:27:37 -0500

    Date format corrected

--- app/models/report.rb
+++ app/models/report.rb
@@ -17,33 +17,33 @@ end
 
 # Rankings
 @@t7months = 7.months.from_now.strftime("%m/%Y")
-@@t8months = 8.months.from_now
-@@t9months = 9.months.from_now
-@@t10months = 10.months.from_now
-@@t11months = 11.months.from_now
-@@t12months = 12.months.from_now
-@@t13months = 13.months.from_now
-@@t14months = 14.months.from_now
-@@t15months = 15.months.from_now
-@@t16months = 16.months.from_now
-@@t17months = 17.months.from_now
-@@t18months = 18.months.from_now
-@@t19months = 19.months.from_now
-@@t20months = 20.months.from_now
-@@t21months = 21.months.from_now
-@@t22months = 22.months.from_now
-@@t23months = 23.months.from_now
-@@t24months = 24.months.from_now
+@@t8months = 8.months.from_now.strftime("%m/%Y")
+@@t9months = 9.months.from_now.strftime("%m/%Y")
+@@t10months = 10.months.from_now.strftime("%m/%Y")
+@@t11months = 11.months.from_now.strftime("%m/%Y")
+@@t12months = 12.months.from_now.strftime("%m/%Y")
+@@t13months = 13.months.from_now.strftime("%m/%Y")
+@@t14months = 14.months.from_now.strftime("%m/%Y")
+@@t15months = 15.months.from_now.strftime("%m/%Y")
+@@t16months = 16.months.from_now.strftime("%m/%Y")
+@@t17months = 17.months.from_now.strftime("%m/%Y")
+@@t18months = 18.months.from_now.strftime("%m/%Y")
+@@t19months = 19.months.from_now.strftime("%m/%Y")
+@@t20months = 20.months.from_now.strftime("%m/%Y")
+@@t21months = 21.months.from_now.strftime("%m/%Y")
+@@t22months = 22.months.from_now.strftime("%m/%Y")
+@@t23months = 23.months.from_now.strftime("%m/%Y")
+@@t24months = 24.months.from_now.strftime("%m/%Y")
 @@lease_to_array = []
 @@lease_to_array_formated = []
 @@available.as_json.each {|i| @@lease_to_array << i["lease_to"] }
-@@lease_to_array.each {|i| @@lease_to_array_formated << i.strftime("%m/%d/%y") if i != nil}
+@@lease_to_array.each {|i| @@lease_to_array_formated << i.strftime("%m/%Y") if i != nil}
 #0002/17
 class Report
 
 
   def self.text(property)
-     "#{@@lease_to_array_formated}"
+     "#{@@t7months}"
   end
 
   end
