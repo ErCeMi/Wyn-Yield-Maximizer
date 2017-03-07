509f9bb4af1a954ddfac8f955b6080a66800e26c
Mario Rosenthal <mario.f.rosenthal@gmail.com>
2017-03-07 10:01:16 -0500

    working admin log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
   def current_user
    @current_user ||= Employee.find(session[:user_id]) if session[:user_id]
  end
@@ -7,6 +8,9 @@ class ApplicationController < ActionController::Base
 
  def authorize
    redirect_to '/login' unless current_user
+ end
+ def is_admin?
+   redirect_to '/properties' unless current_user.admin
  end
 
 end
